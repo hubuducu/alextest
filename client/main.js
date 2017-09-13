0d256f40de7c9663da5ff4ca2748a22c69478608
Hubuducu <verreydt.a@hotmail.com>
2017-09-13 16:11:21 +0200

    first commit

---
+++ client/main.js
@@ -0,0 +1,11 @@
+Template.messagesDisplay.helpers({
+	messagesCount: function () {
+		return 5;
+	},
+	hasMessages: function () {
+		return false;
+	},
+	messages: function (a,b){
+		return ["Rofl","Bien ou bien?"]
+	}
+});
